c10ec3b80d5e9a481989af8311b7657832e163cf
Antonín <av@flwn.dev>
2025-10-14 22:39:52 +0200

    enable refetching data every 10 secs

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -16,7 +16,9 @@ function makeQueryClient() {
                 // With SSR, we usually want to set some default staleTime
                 // above 0 to avoid refetching immediately on the client
                 staleTime: 60 * 1000,
-                refetchInterval: false,
+                // Refetch every 10 seconds
+                refetchInterval: 10 * 1000,
+                refetchOnWindowFocus: true,
                 refetchIntervalInBackground: false,
             },
         },
